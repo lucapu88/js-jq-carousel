67abb8dcbb52c3d4f277e6f9fdf4d5e488e1a9b3
lucapu88 <caputoluca88@gmail.com>
2019-11-27 13:27:23 +0100

    inizio

---
+++ main.js
@@ -0,0 +1,2 @@
+// Provate a riprodurre uno slider di immagini simile a quello visto a lezione, con grafica totalmente a piacere!
+// BONUS: cambiare immagine cliccando sui pallini (mostrando quindi l'immagine con indice corrispondente)
